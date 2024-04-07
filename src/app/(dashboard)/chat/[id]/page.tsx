f8ba2eb3f802c1810631f8d120dbfb9b20733a4f
CRuentgen <ruent.dev@gmail.com>
2024-04-07 23:31:23 +0800

    stable InboxButton

--- src/app/(dashboard)/chat/[id]/page.tsx
+++ src/app/(dashboard)/chat/[id]/page.tsx
@@ -3,7 +3,25 @@ import Message from "@/components/Message/Message";
 import InputMessage from "@/components/Message/InputMessage";
 import HeaderMessage from "@/components/Message/HeaderMessage";
 
-const Chat = () => {
+
+
+async function getMessages() {
+  // const res = await fetch('https://api.example.com/...')
+  // // The return value is *not* serialized
+  // // You can return Date, Map, Set, etc.
+ 
+  // if (!res.ok) {
+  //   // This will activate the closest `error.js` Error Boundary
+  //   throw new Error('Failed to fetch data')
+  // }
+ 
+  // return res.json()
+}
+
+const Chat = async () => {
+
+  const res = await getMessages()
+
   return (
     <div className="single-chat w-full h-screen flex flex-col">
       {/* TOP */}
--- src/components/Sidebar/Components/Buttons/InboxButton.tsx
+++ src/components/Sidebar/Components/Buttons/InboxButton.tsx
@@ -1,26 +1,49 @@
+"use client"
+
+import Image from 'next/image'
 import React from 'react'
 
+type User = {
+  fname: string,
+  lname: string,
+  age: number
+}
+
+interface InboxButtonProps{
+  user: User
+  image?: string
+  isTyping: boolean
+  unreadCount: number
+  time: string
+  content: string
+  onClick?: any
+}
 
 
-
-const InboxButton = () => {
+const InboxButton = (props: InboxButtonProps) => {
+  
   return (
-    <div className='inbox-button w-full h-auto flex justify-between py-2 cursor-pointer'>
+    <div onClick={props.onClick} className='inbox-button w-full h-auto flex justify-between p-2 cursor-pointer ease-in-out duration-300 hover:bg-[#1D1E23]'>
       <div className="image-name-container w-full h-auto flex gap-2">
         {/* Image Container */}
         <div className="image-container w-[50px] h-[50px] flex items-center justify-center relative text-white rounded-full bg-red-500">
-          JD
+          {props.image ? null : `${props.user.fname[0]}${props.user.lname[0]}`.toUpperCase() }
         </div>
         {/* Name Container */}
         <div className="name-container">
-          <p>John Doe</p>
-          <p className='text-xs italic text-green-500'>John Typing...</p>
+          <p className={` text-white ${props.unreadCount > 0 ? "font-[700]" : ""}`}>{props.user.fname} {props.user.lname}</p>
+
+          <p className={`text-xs ${props.isTyping ? "text-green-500 italic": "text-white"}`}>
+           {props.isTyping && `${props.user.fname} is Typing`}
+           {!props.isTyping && `${props.content.substring(0, 20)}...`}
+          </p>
+          
         </div>
       </div>
       {/* Time Container */}
-      <div className="time-container w-auto h-full flex flex-col justify-center items-center">
-        <p className='text-white text-xs block w-[60px] h-full'>12:00 PM</p>
-        <p className='unread-count w-3 h-3 bg-red-500 text-white text-[10px] rounded-full flex items-center justify-center'>2</p>
+      <div className="time-container w-[70px] h-full relative">
+        <p className='text-white text-xs block w-auto h-full float-right'>{props.time}</p>
+        {props.unreadCount > 0 ? <p className='unread-count w-4 h-4 my-2 float-right bg-red-500 text-white text-[10px] rounded-full flex items-center justify-center'>{props.unreadCount}</p> : null}
       </div>
     </div>
   )
