faf61e701110ecd0c8952fef77e1bd27a6e9e35d
CRuentgen <ruent.dev@gmail.com>
2024-04-07 20:19:12 +0800

    creating InboxButton

--- src/components/Sidebar/Components/Buttons/IconButton.tsx
+++ src/components/Sidebar/Components/Buttons/IconButton.tsx
@@ -1,36 +1,25 @@
 "use client"
 import React from 'react'
 import Image from 'next/image'
-import { useRouter } from 'next/navigation'
-import { useDispatch } from 'react-redux'
-import { setSelectedIcon } from '@/app-redux/features/navigationSlice'
+
 
 
 interface IconButtonProps {
   icon: string
   size: number
   label: string
-  link?: string
+  isActive?: boolean
+  onClick?: any
 }
 
 const IconButton = (props: IconButtonProps) => {
 
-  const router = useRouter();
-  const dispatch = useDispatch();
-
-
-  const handleOnClick = (e: any) => {
-    e.preventDefault()
-    dispatch(setSelectedIcon(props.label  ))
-    if(props.link){
-      router.push(props.link)
-    }
-  }
-
   return (
     <button
-      onClick={handleOnClick}
-      className='sidebar-icon-button flex items-center justify-center'
+      title={props.label.toUpperCase()}
+      onClick={props.onClick}
+      aria-label={props.label}
+      className={`sidebar-icon-button flex items-center justify-center p-3 rounded-full ease-in-out duration-300 ${props.isActive && "bg-[#2A9DF4]"} hover:bg-[#2A9DF4]`}
     >
       <Image src={props.icon} alt='button-icon' height={props.size} width={props.size} />
     </button>
--- src/components/Sidebar/Components/Buttons/InboxButton.tsx
+++ src/components/Sidebar/Components/Buttons/InboxButton.tsx
@@ -1,18 +1,27 @@
 import React from 'react'
 
+
+
+
 const InboxButton = () => {
   return (
-    <div className='inbox-button w-full h-auto flex gap-2 py-2'>
-      {/* Image Container */}
-      <div className="image-container w-[50px] h-[50px] flex items-center justify-center relative text-white rounded-full bg-red-500">
-        JD
-      </div>
-      {/* Name Container */}
-      <div className="name-container">
-        <p>John Doe</p>
-        <p className='text-xs italic text-green-500'>John Typing...</p>
+    <div className='inbox-button w-full h-auto flex justify-between py-2 cursor-pointer'>
+      <div className="image-name-container w-full h-auto flex gap-2">
+        {/* Image Container */}
+        <div className="image-container w-[50px] h-[50px] flex items-center justify-center relative text-white rounded-full bg-red-500">
+          JD
+        </div>
+        {/* Name Container */}
+        <div className="name-container">
+          <p>John Doe</p>
+          <p className='text-xs italic text-green-500'>John Typing...</p>
+        </div>
       </div>
       {/* Time Container */}
+      <div className="time-container w-auto h-full flex flex-col justify-center items-center">
+        <p className='text-white text-xs block w-[60px] h-full'>12:00 PM</p>
+        <p className='unread-count w-3 h-3 bg-red-500 text-white text-[10px] rounded-full flex items-center justify-center'>2</p>
+      </div>
     </div>
   )
 }
