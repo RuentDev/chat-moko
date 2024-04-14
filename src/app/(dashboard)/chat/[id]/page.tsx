da5836871a852d801d9c9e07da4a493c3aa62ff3
Your Name <leeryan307@gmail.com>
2024-04-14 21:07:59 +0800

    Create chat settings component

--- src/app/(dashboard)/chat/[id]/page.tsx
+++ src/app/(dashboard)/chat/[id]/page.tsx
@@ -2,36 +2,37 @@ import React from "react";
 import Message from "@/components/Message/Message";
 import InputMessage from "@/components/Message/InputMessage";
 import HeaderMessage from "@/components/Message/HeaderMessage";
-
-
+import ChatSettings from "@/components/Message/ChatSettings";
 
 async function getMessages() {
   // const res = await fetch('https://api.example.com/...')
   // // The return value is *not* serialized
   // // You can return Date, Map, Set, etc.
- 
   // if (!res.ok) {
   //   // This will activate the closest `error.js` Error Boundary
   //   throw new Error('Failed to fetch data')
   // }
- 
   // return res.json()
 }
 
 const Chat = async () => {
-
-  const res = await getMessages()
+  const res = await getMessages();
 
   return (
-    <div className="single-chat w-full h-screen flex flex-col">
-      {/* TOP */}
-      <HeaderMessage />
+    <div className="chat-container flex">
+      <div className="single-chat w-full h-screen flex flex-col">
+        {/* TOP */}
+        <HeaderMessage />
 
-      {/* MESSAGES */}
-      <Message />
+        {/* MESSAGES */}
+        <Message />
 
-      {/* CHAT INPUT */}
-      <InputMessage />
+        {/* CHAT INPUT */}
+        <InputMessage />
+      </div>
+
+      {/* CHAT SETTINGS RIGHT SIDE */}
+      <ChatSettings />
     </div>
   );
 };
