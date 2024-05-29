40ae6849ef4a730f4752363e3954d575bd76ff67
CRuentgen <ruent.dev@gmail.com>
2024-05-29 11:58:17 +0800

    update create new conversation

--- src/components/Messages/NewMessage.tsx
+++ src/components/Messages/NewMessage.tsx
@@ -1,21 +1,34 @@
 "use client";
-import { Container, Flex, HStack, Input, Stack, Text } from "@chakra-ui/react";
-import React, { FC } from "react";
+import { Flex, Input, Text } from "@chakra-ui/react";
+import React, { ChangeEvent, FC } from "react";
 
 interface NewMessageProps {}
 
 const NewMessage: FC<NewMessageProps> = (props) => {
+  const [addUserInput, setAddUserInout] = React.useState("");
+  const [addUsers, setAddUsers] = React.useState<string[]>([]);
+
+  const handleUserInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value
+    setAddUserInout(value)
+  }
+
   return (
     <Flex width="100%" height="100vh" flexDirection="column" padding="1rem">
-      <Flex alignItems="center" justifyContent="center" gap="1">
-        <Text>To: </Text>
-        <Input
-          value=""
-          size='md'
-          autoFocus
-          onChange={() => {}}
-        />
-      </Flex>
+      <form action="">
+        <Flex alignItems="center" justifyContent="center" gap="1">
+          <Text width="auto">To: asdasd</Text>
+          <Input
+            value={addUserInput}
+            size='md'
+            autoFocus
+            type="text"
+            onChange={handleUserInputChange}
+            // border={"none"}
+            // outline={"none"}
+          />
+        </Flex>
+      </form>
     </Flex>
   );
 };
